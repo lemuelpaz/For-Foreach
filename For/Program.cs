f9015221c38049492af6f24d3a0c5498e5f2ae85
lemuelpaz <lemuelpazcontato@hotmail.com>
2022-05-18 22:18:14 -0300

    for-foreach

---
+++ For/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace For
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                //Console.WriteLine(i);
+            }
+
+            string[] valores = { "Vinte", "Trinta", "Quarenta" };
+
+            foreach (string y in valores)
+            {
+                Console.WriteLine(y);
+            }
+        }
+    }
+}
